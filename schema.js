fbae18d4f532d225e3d191244885e10296856090
root <gogia.prateek@hotmail.com>
2018-06-04 10:34:22 +0530

    Implemented RootQuery

--- schema.js
+++ schema.js
@@ -7,26 +7,67 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
-// Customer Type
+// Hardcoded data
+const customers = [{
+        id: '1',
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        age: 35
+    },
+    {
+        id: '2',
+        name: 'Steve Smith',
+        email: 'steve@gmail.com',
+        age: 25
+    },
+    {
+        id: '3',
+        name: 'Sarrah Williams',
+        email: 'sarrah@gmail.com',
+        age: 30
+    }
+]
 
+// Customer Type
 const CustomerType = new GraphQLObjectType({
     name: 'Customer',
     fields: () => ({
-        id: {type: GraphQLString},
-        name: {type: GraphQLString},
-        email: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        id: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        },
+        email: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        }
     })
-});
+})
+
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
-    customer: {
-        type: CutomerType
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parentValue, args) {
+                for (let i = 0; i < customers.length; i++) {
+                    if (customers[i].id == args.id) {
+                        return customers[i];
+                    }
+                }
+            }
+        }
     }
 });
 
 module.exports = new GraphQLSchema({
-
+    query: RootQuery
 });
